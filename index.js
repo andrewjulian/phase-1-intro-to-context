6fa1bd7ca0f89311a477c35b6b8739e00896e93f
+ <+>
2022-03-06 15:44:57 -0700

    Just the Calculations left...

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 // Your code here
 
-const employeeRecords = [
-    ["moe", "sizlak", "barkeep", 2],
-    ["bartholomew", "simpson", "scamp", 3]
-  ];
-
 //create a record for an employee (no time data included)
 function createEmployeeRecord(employeeInfoArray){
     return {
@@ -17,24 +12,73 @@ function createEmployeeRecord(employeeInfoArray){
     }
 }
 
+
+
 //create employee records from an array of arrays (employee data)
 function createEmployeeRecords(array) {
-    
-    for(item of array){
+    const employeeRecords = []
+    for(let item of array){
         employeeRecords.push(createEmployeeRecord(item))
     }
-    console.log(employeeRecords.length)
     return employeeRecords;
 }
 
-//creates a time object and is added to the employee object
-function createTimeInEvent(employeeObject, date){
-    employeeObject.timeInEvents = {
-        'type': 'TimeIn',
-        'hour': date.slice(11,13),
-        'date': date.slice(0,10)
+
+//creates a time in object and is added to the employee object
+function createTimeInEvent(employeeObject, workDay){
+    const totalEventsIn = employeeObject.timeInEvents 
+    const newInput = {
+        type: 'TimeIn',
+        hour: parseInt(workDay.slice(11,13)+"00"),
+        date: workDay.slice(0,10)
     }
+    totalEventsIn.push(newInput)
     return employeeObject;
 }
 
-//console.log(createTimeInEvent(employeeRecords[0], "1987-05-27 1250"))
+//creates a time out object and is added to the employee object
+function createTimeOutEvent(employeeObject, workDay){
+    const totalEventsOut = employeeObject.timeOutEvents
+    const newInput = {
+        type: 'TimeOut',
+        hour: parseInt(workDay.slice(11,13)+"00"),
+        date: workDay.slice(0,10) 
+    }
+    totalEventsOut.push(newInput)
+    return employeeObject;
+}
+
+//calculates the hours worked for a single employee on a specific day
+function hoursWorkedOnDate (employeeObject, workDay){
+    for(let day of employeeObject.timeInEvents){
+        if(day.date == workDay){
+            return day.timeOutEvents.hour - day.timeInEvents.hour
+        }
+    }
+    
+}
+
+//calculates the pay (hours * payrate) for a single employee on a specific day
+function wagesEarnedOnDate (employeeObject, workDay){
+    return hoursWorkedOnDate(employeeObject, workDay) * employeeObject.payPerHour
+}
+
+//clculates the pay for a single employee for all days worked
+function allWagesFor(employeeObject){
+    //for all dates worked, total wages
+    let totalPay = 0;
+    for(let day in employeeObject.timeInEvents.date){
+        totalPay += hoursWorkedOnDate(employeeObject, workDay)
+    }
+    return totalPay
+}
+
+//calculates the total payroll for all employees on all days
+function calculatePayroll(array){
+    let totalPayroll = 0;
+    for(let person in array){
+        totalPayroll += allWagesFor(person)
+    }
+    return totalPayroll
+}
+
