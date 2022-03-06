5645babfee8c2c829cc6a78982fe721419d4c6c5
+ <+>
2022-03-06 13:06:13 -0700

    Functions seem right, but the tests are not indicating correct

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+const employeeRecords = [
+    ["moe", "sizlak", "barkeep", 2],
+    ["bartholomew", "simpson", "scamp", 3]
+  ];
+
+//create a record for an employee (no time data included)
+function createEmployeeRecord(employeeInfoArray){
+    return {
+        'firstName' : employeeInfoArray[0],
+        'familyName': employeeInfoArray[1],
+        'title': employeeInfoArray[2],
+        'payPerHour': employeeInfoArray[3],
+        'timeInEvents': [],
+        'timeOutEvents': []
+    }
+}
+
+//create employee records from an array of arrays (employee data)
+function createEmployeeRecords(array) {
+    
+    for(item of array){
+        employeeRecords.push(createEmployeeRecord(item))
+    }
+    console.log(employeeRecords.length)
+    return employeeRecords;
+}
+
+//creates a time object and is added to the employee object
+function createTimeInEvent(employeeObject, date){
+    employeeObject.timeInEvents = {
+        'type': 'TimeIn',
+        'hour': date.slice(11,13),
+        'date': date.slice(0,10)
+    }
+    return employeeObject;
+}
+
+//console.log(createTimeInEvent(employeeRecords[0], "1987-05-27 1250"))
