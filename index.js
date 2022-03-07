f392c25f0192ae129577276c1f677e08d79f93f1
+ <+>
2022-03-07 13:17:03 -0700

    All Tests Passing

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Your code here
-
 //create a record for an employee (no time data included)
 function createEmployeeRecord(employeeInfoArray){
     return {
@@ -12,25 +10,19 @@ function createEmployeeRecord(employeeInfoArray){
     }
 }
 
-
-
 //create employee records from an array of arrays (employee data)
 function createEmployeeRecords(array) {
-    const employeeRecords = []
-    for(let item of array){
-        employeeRecords.push(createEmployeeRecord(item))
-    }
+    const employeeRecords = array.map(createEmployeeRecord)
     return employeeRecords;
 }
-
 
 //creates a time in object and is added to the employee object
 function createTimeInEvent(employeeObject, workDay){
     const totalEventsIn = employeeObject.timeInEvents 
     const newInput = {
-        type: 'TimeIn',
-        hour: parseInt(workDay.slice(11,13)+"00"),
-        date: workDay.slice(0,10)
+        'type': 'TimeIn',
+        'hour': parseInt(workDay.slice(11,13)+"00"),
+        'date': workDay.slice(0,10)
     }
     totalEventsIn.push(newInput)
     return employeeObject;
@@ -40,22 +32,26 @@ function createTimeInEvent(employeeObject, workDay){
 function createTimeOutEvent(employeeObject, workDay){
     const totalEventsOut = employeeObject.timeOutEvents
     const newInput = {
-        type: 'TimeOut',
-        hour: parseInt(workDay.slice(11,13)+"00"),
-        date: workDay.slice(0,10) 
+        'type': 'TimeOut',
+        'hour': parseInt(workDay.slice(11,13)+"00"),
+        'date': workDay.slice(0,10) 
     }
     totalEventsOut.push(newInput)
     return employeeObject;
 }
 
 //calculates the hours worked for a single employee on a specific day
-function hoursWorkedOnDate (employeeObject, workDay){
-    for(let day of employeeObject.timeInEvents){
-        if(day.date == workDay){
-            return day.timeOutEvents.hour - day.timeInEvents.hour
+function hoursWorkedOnDate(employeeObject, workDay){
+    let totalHours = 0
+    const employeeTimeInList = employeeObject.timeInEvents
+    console.log(employeeTimeInList)
+    for(let day in employeeTimeInList){
+        console.log(day)
+        if(employeeObject.timeInEvents[day].date === workDay){
+            totalHours += employeeObject.timeOutEvents[day].hour - employeeObject.timeInEvents[day].hour
         }
     }
-    
+    return totalHours/100
 }
 
 //calculates the pay (hours * payrate) for a single employee on a specific day
@@ -63,22 +59,98 @@ function wagesEarnedOnDate (employeeObject, workDay){
     return hoursWorkedOnDate(employeeObject, workDay) * employeeObject.payPerHour
 }
 
+
 //clculates the pay for a single employee for all days worked
 function allWagesFor(employeeObject){
     //for all dates worked, total wages
     let totalPay = 0;
-    for(let day in employeeObject.timeInEvents.date){
-        totalPay += hoursWorkedOnDate(employeeObject, workDay)
+    const employeeTimeInList = employeeObject.timeInEvents
+    for(let day in employeeTimeInList){
+        console.log(day)
+        totalPay += wagesEarnedOnDate(employeeObject, employeeObject.timeInEvents[day].date)
     }
     return totalPay
 }
 
 //calculates the total payroll for all employees on all days
 function calculatePayroll(array){
-    let totalPayroll = 0;
-    for(let person in array){
+    let totalPayroll = 0;  
+
+    for(let person of array){
+        console.log(person)
         totalPayroll += allWagesFor(person)
-    }
+    } 
+
     return totalPayroll
 }
 
+
+let employee = {
+    'firstName' : "Thor",
+    'familyName': "Odinsson",
+    'title': "Electrical Engineer",
+    'payPerHour': 45,
+    'timeInEvents': [{
+        type: 'TimeIn',
+        hour: 1600,
+        date: "2018-01-01"
+    }],
+    'timeOutEvents': [{
+        type: 'TimeOut',
+        hour: 1800,
+        date: "2018-01-01"
+    }]
+}
+
+const employees = [
+    {
+        'firstName' : "Thor",
+        'familyName': "Odinsson",
+        'title': "Electrical Engineer",
+        'payPerHour': 45,
+        'timeInEvents': [{
+            type: 'TimeIn',
+            hour: 1600,
+            date: "2018-01-01"
+        }],
+        'timeOutEvents': [{
+            type: 'TimeOut',
+            hour: 1800,
+            date: "2018-01-01"
+        }]
+    },
+
+    {
+        'firstName' : "Thor",
+        'familyName': "Odinsson",
+        'title': "Electrical Engineer",
+        'payPerHour': 45,
+        'timeInEvents': [{
+            type: 'TimeIn',
+            hour: 1600,
+            date: "2018-01-01"
+        }],
+        'timeOutEvents': [{
+            type: 'TimeOut',
+            hour: 1800,
+            date: "2018-01-01"
+        }]
+    },
+
+    {
+        'firstName' : "Thor",
+        'familyName': "Odinsson",
+        'title': "Electrical Engineer",
+        'payPerHour': 45,
+        'timeInEvents': [{
+            type: 'TimeIn',
+            hour: 1600,
+            date: "2018-01-01"
+        }],
+        'timeOutEvents': [{
+            type: 'TimeOut',
+            hour: 1800,
+            date: "2018-01-01"
+        }]
+    }
+]
